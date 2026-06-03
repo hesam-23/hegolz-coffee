91f4984c835afdd76d3dd3a13b26aeb76bc22194
hesam-23 <en_concrete@yahoo.com>
2026-06-03 13:15:46 -0400

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,9 @@ import '../styles/Header.css';
 function Header({ onShowHistory, orderCount }) {
   return (
     <div className="header">
-      <img src="/27.svg" alt="Hegolz Coffee" className="header-logo" />
       <div className="header-info">
-        <h1 className="header-title">Hegolz Coffee</h1>
-        <p className="header-subtitle">Modern Cafe Experience</p>
+        <h1 className="header-title">HEGOLZ</h1>
+        <p className="header-subtitle">Real Time Order System</p>
       </div>
       <button className="history-btn" onClick={onShowHistory}>
         Orders
